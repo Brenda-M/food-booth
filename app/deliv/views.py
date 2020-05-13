25134062546673ac1863c31f4d3ffe2d707589be
Brenda-M <brendanjuguna1@gmail.com>
2020-05-14 00:16:16 +0300

    populate the form with the users phone number

--- app/deliv/views.py
+++ app/deliv/views.py
@@ -1,8 +1,9 @@
 from . import deliv
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from flask_login import login_required
 from app.models import Menu, Address, User
 from .forms import Delivery
+from .email import order_email
 
 @deliv.route('/menu')
 def main():
@@ -21,11 +22,16 @@ def deliv_info():
     db.session.add(deliv_address)
     db.session.commit()
 
+    current_user.phone_number = form.phone_number.data 
+
   users = User.query.all()
     for user in users:
       if user.email == current_user.email:
         delivery_email("Order Confirmation", "email/new_order", user.email, user=user)
 
-        flash('Your order has been received! You will receive a confirmation email shortly', 'success')
-
-        
+    flash('Your order has been received! You will receive a confirmation email shortly', 'success')
+    
+    return redirect(url_for('.main'))
+  
+  return render_template('delivery_info.html', title="Delivery Details", form=form)
+  
