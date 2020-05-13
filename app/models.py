27a092d40fd5477761915e163ead6bf540b486a2
hamisicodes <newhamisi@gmail.com>
2020-05-13 10:29:15 +0300

    Add basic models

---
+++ app/models.py
@@ -0,0 +1,55 @@
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer ,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255),unique = True, index = True)
+    pass_secure = db.Column(db.String(255))
+    phone_number = db.Column(db.Integer)
+
+    orders  = db.relationship('Order', backref = 'user' , lazy = 'dynamic')
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def __repr__(self):
+         return f'User {self.username}'
+
+
+class Menu(db.Model):
+    __tablename__ = 'menus'
+
+    id = db.Column(db.Integer ,primary_key = True)
+    name = db.Column(db.String(255), index = True)
+    description =  db.Column(db.String(255), index = True)
+    price = db.Column(db.Integer)
+    orders  = db.relationship('Order', backref = 'menu' , lazy = 'dynamic')
+
+
+class Service(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer ,primary_key = True)
+    name =  db.Column(db.String(255), index = True)
+    price = db.Column(db.Integer)
+    orders  = db.relationship('Order', backref = 'service' , lazy = 'dynamic')
+    
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer ,primary_key = True)
+    user_id =  db.Column(db.Integer, db.ForeignKey('users.id'))
+    menu_id = db.Column(db.Integer, db.ForeignKey('menus.id'))
+    service_id =  db.Column(db.Integer, db.ForeignKey('services.id'))
