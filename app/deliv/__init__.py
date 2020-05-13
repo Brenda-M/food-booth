40e023d593fafca425a3cb89e407cc04c26a8e9b
Brenda-M <brendanjuguna1@gmail.com>
2020-05-13 17:04:27 +0300

    register a delivery blueprint

--- app/deliv/__init__.py
+++ app/deliv/__init__.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
 
+<<<<<<< HEAD
 deliv = Blueprint('deliv', __name__)
+=======
+deliv = ('deliv', __name__)
+>>>>>>> d3ced29... register a delivery blueprint
 
 from . import views
