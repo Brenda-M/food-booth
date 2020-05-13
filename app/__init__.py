427506f9cf91cc8b17351839335686aaff177ae2
glasses-png <cchelangat16@gmail.com>
2020-05-13 15:49:46 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_bootsrap import Bootstrap
 
 
 
@@ -31,9 +32,6 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
-
-    # configure UploadSet
-    configure_uploads(app,photos)
 
     # initialiaze the database
     db.init_app(app)
