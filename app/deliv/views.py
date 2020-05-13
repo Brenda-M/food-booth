fbd105a1bc7c503d8b0bff203e873bc837c33e0a
Brenda-M <brendanjuguna1@gmail.com>
2020-05-14 00:07:59 +0300

    create a delivery address object once the form validates

--- app/deliv/views.py
+++ app/deliv/views.py
@@ -1,6 +1,8 @@
 from . import deliv
-from flask import render_template
-from app.models import Menu
+from flask import render_template, url_for
+from flask_login import login_required
+from app.models import Menu, Address, User
+from .forms import Delivery
 
 @deliv.route('/menu')
 def main():
@@ -8,3 +10,14 @@ def main():
   menus = Menu.query.all()
 
   return render_template('menu.html', title = "Food gallery", items=menus)
+
+@deliv.route('/delivery')
+def deliv_info():
+
+  form = Delivery()
+
+  if form.validate_on_submit():
+    deliv_address = Address(phonenumber=current_user.phone_number, street_address=form.street_address.data, postal_code=form.postal_code.data, city=form.city.data)
+    db.session.add(deliv_address)
+    db.session.commit()
+
