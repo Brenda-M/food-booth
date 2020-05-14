0e318054efa16526a6487853403554e2f71c5460
Brenda-M <brendanjuguna1@gmail.com>
2020-05-14 17:44:46 +0300

    add cart

--- app/deliv/forms.py
+++ app/deliv/forms.py
@@ -1,11 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_login import current_user
 from app.models import Address
 
 
 class Delivery(FlaskForm):
+  phone_number = IntegerField('Phone Number', validators=[DataRequired()])
   street_address = StringField('Address', validators=[DataRequired()])
   postal_code = StringField('Postal Address', validators=[DataRequired()])
   city = StringField('City', validators=[DataRequired()])
--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
 from flask_mail import Message
+<<<<<<< HEAD
 from . import mail 
 from flask import render_template
 
@@ -9,3 +10,15 @@ def mail_message(subject,template,to,**kwargs):
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
+=======
+from flask import render_template
+from . import mail
+
+def order_email(subject, template, to, **kwargs):
+  sender_email = 'noreplaymail84@gmail.com'
+
+  email = Message(subject, sender=sender_email, recipients=[to])
+  email.body= render_template(template + ".txt",**kwargs)
+  email.html = render_template(template + ".html",**kwargs)
+  mail.send(email)
+>>>>>>> 0a369e4... add cart
