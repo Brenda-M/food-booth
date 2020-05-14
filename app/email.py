143e73369a5e034704daa360a748e4162630802c
Brenda-M <brendanjuguna1@gmail.com>
2020-05-14 19:17:57 +0300

    update requirements file

--- app/email.py
+++ app/email.py
@@ -1,5 +1,4 @@
 from flask_mail import Message
-<<<<<<< HEAD
 from . import mail 
 from flask import render_template
 
@@ -10,9 +9,6 @@ def mail_message(subject,template,to,**kwargs):
     email.body= render_template(template + ".txt",**kwargs)
     email.html = render_template(template + ".html",**kwargs)
     mail.send(email)
-=======
-from flask import render_template
-from . import mail
 
 def order_email(subject, template, to, **kwargs):
   sender_email = 'noreplaymail84@gmail.com'
@@ -21,4 +17,3 @@ def order_email(subject, template, to, **kwargs):
   email.body= render_template(template + ".txt",**kwargs)
   email.html = render_template(template + ".html",**kwargs)
   mail.send(email)
->>>>>>> 0a369e4... add cart
